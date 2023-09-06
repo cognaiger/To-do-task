e57d8e291f1dcde5f6015b1a450f1aabfdc90428
cognaiger <21020369@vnu.edu.vn>
2023-09-06 23:23:10 +0700

    client to server sign up

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,32 @@
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [msg, setMsg] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = async(e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:5000/auth/login", {
+        email: email,
+        password: password
+      });
+
+      if (response.status === 201) {
+        navigate("/");
+      } else {
+        setMsg("Login failed, please try again!");
+      }
+    } catch (err) {
+      setMsg("Login failed, please try again!");
+    }
+  }
+
     return (
         <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
             <div class="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -9,13 +35,13 @@ const Login = () => {
             </div>
 
             <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-              <div class="space-y-6">
+              <form class="space-y-6" onSubmit={handleLogin}>
                 <div>
                   <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
                   <div class="mt-2">
                     <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md 
                     border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 
-                    focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={email} onChange={(e) => setEmail(e.target.value)} />
                   </div>
                 </div>
 
@@ -31,14 +57,21 @@ const Login = () => {
                   <div class="mt-2">
                     <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 
                     py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 
-                    sm:text-sm sm:leading-6" />
+                    sm:text-sm sm:leading-6" value={password} onChange={(e) => setPassword(e.target.value)} />
                   </div>
                 </div>
 
-                <div>
-                  <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+                <div class="mt-10 text-center leading-9 tracking-tight text-gray-900">
+                  {msg ? <p>{msg}</p> : null}
                 </div>
-              </div>
+
+                <div>
+                  <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white 
+                  shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    Sign in
+                  </button>
+                </div>
+              </form>
 
               <p class="mt-10 text-center text-sm text-gray-500">
                 Not a member?
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,37 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 const Register = () => {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [repassword, setRePassword] = useState('');
+  const [msg, setMsg] = useState('');
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:5000/auth/register", {
+        email: email,
+        username: username,
+        password: password,
+        repassword: repassword
+      });
+
+      if (response.status === 201) {
+        navigate("/login");
+      } else {
+        setMsg("Register fail, please try again!");
+      }
+    } catch (err) {
+      setMsg("Register fail, please try again");
+    }
+  }
+
+
     return (
         <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
             <div class="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -9,13 +40,22 @@ const Register = () => {
             </div>
 
             <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-              <div class="space-y-6">
+              <form class="space-y-6" onSubmit={handleRegister}>
                 <div>
                   <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
                   <div class="mt-2">
                     <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md 
                     border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 
-                    focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={email} onChange={(e) => setEmail(e.target.value)} />
+                  </div>
+                </div>
+
+                <div>
+                  <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
+                  <div class="mt-2">
+                    <input id="username" name="username" type="text" required class="block w-full rounded-md 
+                    border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 
+                    focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={username} onChange={(e) => setUsername(e.target.value)} />
                   </div>
                 </div>
 
@@ -26,7 +66,7 @@ const Register = () => {
                   <div class="mt-2">
                     <input id="password" name="password" type="password" required class="block w-full rounded-md border-0 
                     py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 
-                    sm:text-sm sm:leading-6" />
+                    sm:text-sm sm:leading-6" value={password} onChange={(e) => setPassword(e.target.value)} />
                   </div>
                 </div>
 
@@ -35,17 +75,22 @@ const Register = () => {
                   <div class="mt-2">
                     <input id="repassword" name="password" type="password" required class="block w-full rounded-md 
                     border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 
-                    focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={repassword} onChange={(e) => setRePassword(e.target.value)} />
                   </div>
                 </div>
 
+                <div class="mt-10 text-center leading-9 tracking-tight text-gray-900">
+                  {msg ? <p>{msg}</p> : null}
+                </div>
+
                 <div>
-                  <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white 
-                  shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                  <button class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm 
+                  hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  type="submit">
                     Register
                   </button>
                 </div>
-              </div>
+              </form>
 
               <p class="mt-10 text-center text-sm text-gray-500">
                 Have already be a member?
